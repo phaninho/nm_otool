c77bdf87b5234ec15b914e0ff5320139ef7f7dcf
phaninho <phaninho@live.fr>
2018-02-10 03:15:54 +0100

    test display some struct values

--- ft_nm.c
+++ ft_nm.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
+
 #include <mach-o/loader.h>
 #include <mach-o/nlist.h>
 
@@ -16,9 +18,19 @@ void    print_output(int nsyms, int symoff, int stroff, void *ptr)
 
   array = ptr + symoff;
   strtab = ptr + stroff;
-  i = 0;
+  i =-1;
   while (++i < nsyms)
-    printf("[%s]\n", strtab + array[i].n_un.n_strx);
+  {
+    printf("%s ", strtab + array[i].n_type);
+    // if (strtab + array[i].n_type && !strcmp(strtab + array[i].n_type,  "assign_block"))
+    //   printf("T ");
+    // else if (!strcmp(strtab + array[i].n_type,  "_assign_block"))
+    // printf("d ");
+    // else
+    //   printf("U ");
+    printf("[%s]\n",strtab + array[i].n_un.n_strx);
+  }
+
 }
 
 
@@ -40,6 +52,8 @@ void handle_64(void *ptr)
     if (lc->cmd == LC_SYMTAB)
     {
       sym = (struct symtab_command *)lc;
+      uint32_t cmd;
+   printf("\ncmdsize=%d\n symoff=%d\n nsyms=%d\n stroff=%d\n strsize=%d\n\n",sym->cmdsize,  sym->symoff, sym->nsyms, sym->stroff,  sym->strsize);
       print_output(sym->nsyms, sym->symoff, sym->stroff, ptr);
       // printf("c'est le bon %d\n", sym->nsyms);
       break ;
@@ -77,6 +91,15 @@ int main(int ac, char **av)
       return (printf("Open error\n"));
     if (fstat(fd, &buff) < 0)
       return (printf("fstat error\n"));
+      printf("/// st_dev  %d\n", buff.st_dev);
+      printf("/// st_ino  %llu\n", buff.st_ino);
+      printf("/// st_mode  %d\n", buff.st_mode);
+      printf("/// st_nlink %d\n", buff.st_nlink);
+      printf("/// st_uid  %d\n", buff.st_uid);
+      printf("/// st_gid  %d\n", buff.st_gid);
+      printf("/// st_rdev %d\n", buff.st_rdev);
+      printf("/// st_size %lld\n", buff.st_size);
+
     if ((ptr = mmap(0, buff.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
       return (printf("mmap error\n"));
     nm(ptr);
