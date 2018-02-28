213190bc93e95677e37a4b8e10d39cbe7a7e85ac
Stephane MARTINS <stmartin@e2r4p23.42.fr>
2018-02-28 11:40:50 +0100

    [update] check open and fstat in same condition

--- src/ft_nm.c
+++ src/ft_nm.c
@@ -12,36 +12,6 @@
 
 #include "../include/nm_otool.h"
 
-// char    define_symbol_type(char c, int addr_value, char *sct, char *seg)
-// {
-//   if ((c & N_TYPE) == N_UNDF)
-//   {
-//     c = 'u';
-//     if (addr_value != 0)
-//       c = 'c';
-//   }
-//   else if ((c & N_TYPE) == N_PBUD)
-//      c = 'u';
-//   else if ((c & N_TYPE) == N_ABS)
-//     c = 'a';
-//   else if ((c & N_TYPE) == N_SECT)
-//   {
-//     if (ft_strcmp(sct, SECT_TEXT) == 0 && ft_strcmp(seg, SEG_TEXT) == 0)
-//       c = 't';
-//     else if (ft_strcmp(sct, SECT_DATA) == 0 && ft_strcmp(seg, SEG_DATA) == 0)
-//       c = 'd';
-//     else if (ft_strcmp(sct, SECT_BSS) == 0 && ft_strcmp(seg, SEG_DATA) == 0)
-//       c = 'b';
-//     else
-//       c = 's';
-//   }
-//   else if ((c & N_TYPE) == N_INDR)
-//     c = 'i';
-//   else
-//     c = '?';
-//   return (c);
-// }
-
 void  nm(void *ptr)
 {
   int   magic_number;
@@ -57,25 +27,20 @@ int	 read_args(char *av)
 {
   int         fd;
   char        *ptr;
-  struct stat buff;
+  struct stat buf;
 
-  if ((fd = open(av, O_RDONLY)) <= 0)
+  if ((fd = open(av, O_RDONLY)) <= 0 || (fstat(fd, &buff) < 0))
   {
-    ft_putendl("Open error");
+    ft_putendl("Open or fstat error");
     return (1);
   }
-  if (fstat(fd, &buff) < 0)
-  {
-    ft_putendl("fstat error");
-    return (1);
-  }
-  if ((ptr = mmap(0, buff.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
+  if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
   {
     ft_putendl("mmap error");
     return (1);
   }
   nm(ptr);
-  if (munmap(ptr, buff.st_size) < 0)
+  if (munmap(ptr, buf.st_size) < 0)
   {
     ft_putendl("munmap error");
     return (1);
