f06d69bacf2237e4cee4d7bca7d8bc6a79874c35
phaninho <phaninho@live.fr>
2018-02-28 22:51:18 +0100

    [add] display elements from fat to learn fat structure

--- src/manage_fat.c
+++ src/manage_fat.c
@@ -23,19 +23,25 @@ void handle_fat(void *ptr, int o)
 {
   t_envfat e;
   // struct fat_header *header;
-int archn;
+int arch_n;
 (void)o;
   // e = init_env_fat();
   e.header = (struct fat_header *)ptr;
-  archn = e.header->nfat_arch;
+  arch_n = e.header->nfat_arch;
   if (e.header->magic == FAT_CIGAM)
   {
-    archn = swap_endian(e.header->nfat_arch);
-    printf("sw => [%d]\n", archn);
+    arch_n = swap_endian(e.header->nfat_arch);
+    printf("sw => [%d]\n", arch_n);
   }
   e.lc = ptr + sizeof(struct fat_header);
-  archn = swap_endian(e.lc->cputype);
-  printf("[%d]\n", archn);
+  // archn = swap_endian(e.lc->cputype);
+  printf("cputype[%d] offset %d size %d\n", swap_endian(e.lc->cputype), swap_endian(e.lc->offset), swap_endian(e.lc->size));
+  if (swap_endian(e.lc->cputype) == CPU_TYPE_I386)
+    printf("ok\n");
+  e.lc = (void *)e.lc + sizeof(struct fat_arch);
+  printf("cputype[%d] offset %d size %d\n", swap_endian(e.lc->cputype), swap_endian(e.lc->offset), swap_endian(e.lc->size));
+  if (swap_endian(e.lc->cputype) == CPU_TYPE_X86_64)
+    printf("ok\n");
   // while (e.i++ < (int)e.header->ncmds)
   // {
   //   if (e.lc->cmd == LC_SEGMENT)
